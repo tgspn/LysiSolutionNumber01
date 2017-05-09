8f0990e5e9d769b5ad28b0413335df4f50aac134
Tiago <tgspn@users.noreply.github.com>
2017-05-08 22:58:34 -0300

    Update ProdutoRepository.cs
    Refatorados o código, adicionado o método de AddGroup, neste método você irá adicionar um produto para o grupo.

--- LysiSolutionNumber01/Models/Repository/ProdutoRepository.cs
+++ LysiSolutionNumber01/Models/Repository/ProdutoRepository.cs
@@ -12,37 +12,46 @@ namespace LysiSolutionNumber01.Models.Repository
         public ProdutoRepository(ProdutoContext context)
         {
             _context = context;
-
-            if (_context.Registros.Count() == 0)
-                Add(new Produto { Nome = "Teste", GrupoId = 1 });
+            
+            if (_context.Produto.Count() == 0)//Eu acho que isso é falho, pois você só irá conseguir adicionar um único produto.
+                Add(new Produto { Nome = "Teste" });
         }
 
         public IEnumerable<Produto> GetAll()
         {
-            return _context.Registros.Include(p => p.Grupo).ToList();
+            return _context.Produtos.Include(p => p.Grupo).ToList();
         }
 
         public void Add(Produto item)
         {
-            _context.Registros.Add(item);
+            var grupo=_context.Grupos.Find(1);
+            if(grupo!=null)
+               AddToGroup(item,grupo);
+            
+        }
+        public void AddToGroup(Produto item,LysiSolutionNumber01.Models.Grupo grupo)
+        {
+              if(grupo.Produtos==null)
+                 grupo.Produtos=new List<Produtos>();
+              ggrupo.Produtos.Add(item);
+              
             _context.SaveChanges();
         }
-
         public Produto Find(long key)
         {
-            return _context.Registros.Include("Grupo").FirstOrDefault(t => t.Id == key);
+            return _context.Produtos.Include("Grupo").FirstOrDefault(t => t.Id == key);
         }
 
         public void Remove(long key)
         {
-            var entity = _context.Registros.First(t => t.Id == key);
-            _context.Registros.Remove(entity);
+            var entity = _context.Produtos.First(t => t.Id == key);
+            _context.Produtos.Remove(entity);
             _context.SaveChanges();
         }
 
         public void Update(Produto item)
         {
-            _context.Registros.Update(item);
+            _context.Produtos.Update(item);
             _context.SaveChanges();
         }
     }
